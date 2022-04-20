c1d15583d8e0cbfd986aab23c6ca362d51c9a06b
Hayko Balagyozyan <hayko.balagyozyan@gmail.com>
2022-04-20 03:40:00 -0700

    conenct backend

--- cards/src/Provider/index.js
+++ cards/src/Provider/index.js
@@ -1,28 +1,57 @@
 import React, { useState, createContext } from "react";
+import axios from "axios";
 
 export const DivsContext = createContext();
 
 const DivsProvider = ({ children }) => {
-  const [number, setNumber] = useState([4, 4, 5]);
+  const [number, setNumber] = useState([]);
 
-  const add = () => {
-    number.push(Math.floor(Math.random() * 100));
-    setNumber([...number]);
+  const axiosInstance = axios.create({
+    baseURL: "http://localhost:5000/api",
+  });
+
+  const add = async () => {
+    try {
+      const response = await axiosInstance.post("/");
+      const allNumbers = [...number];
+      allNumbers.push(response.data[0]);
+      setNumber(allNumbers);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getNumbers = () => {
+    axiosInstance
+      .get("/")
+      .then((response) => {
+        setNumber(response.data);
+      })
+      .catch((e) => console.log("Error"));
   };
 
   const sort = () => {
-    number.sort((a, b) => a - b);
+    number.sort((a, b) => a.random[0] - b.random[0]);
     setNumber([...number]);
   };
 
-  const deleteNumber = (specialIndex) => {
-    setNumber(number.filter((_, index) => specialIndex !== index));
+  const deleteNumber = async (specialIndex) => {
+    try {
+      const response = await axiosInstance.delete(`/${specialIndex}`);
+      const NumberFilter = number.filter(
+        (item) => item.id !== response.data[0].id
+      );
+      setNumber([...NumberFilter]);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <DivsContext.Provider
       value={{
         number,
+        getNumbers: getNumbers,
         addNumber: add,
         sortNumbers: sort,
         deleteNumber: deleteNumber,
